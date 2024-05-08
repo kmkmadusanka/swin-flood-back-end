ee7b01dab4abb812f5a68751d478a0093028b148
Dinuka Navaratna <dinuka.navaratna@gmail.com>
2024-05-09 07:21:01 +0800

    Admin 1

--- src/flood.py
+++ src/flood.py
@@ -28,12 +28,10 @@ class SafeLocations(Resource):
             logger.info("Safe locations failed \n"+str(db_response))
             return {"response": "failed", "message": "Safe locations not found!", "description": str(db_response)}, 200
         
-        # return {"response": "success", "locations": [{"geo": [0.0, 0.0], "address": "address", "distance":"0km"},{"geo": [0.0, 0.0], "address": "address", "distance":"0km"}]}, 200
 
     def post(self):
         logger.debug("------------------------------------------------")
         logger.info('/SafeLocations (Post) - '+str(request.remote_addr))
-        # return {"response": "success", "message": "Safe location recorded successfully!"}, 200
         try:
             request_body = request.json
             geo = request_body["geo"]
@@ -62,7 +60,50 @@ class SafeLocations(Resource):
 class FloodHistory(Resource):
     def get(self):
         logger.debug("------------------------------------------------")
-        logger.info('/SafeLocations - '+str(request.remote_addr))
+        logger.info('/FloodHistory (Get) - '+str(request.remote_addr))
+
+        db_response = Select("id, date, geo_lat, geo_lng, rainfall, flood", "history", "", 0)
+
+        if(db_response == None):
+            return {"response": "failed", "message": "Flood history not found!"}, 200
+        elif(type(db_response) is list):
+            response = {}
+            response["response"] = "success"
+            locations = []
+            for location in db_response:
+                locations.append({"id":str(location[0]), "date":str(location[1]), "geo":[float(location[2]), float(location[3])], "rainfall":str(location[4]), "flood":str(location[5])})
+            response["locations"] = locations
+            return response
+        else:
+            logger.info("Flood history failed \n"+str(db_response))
+            return {"response": "failed", "message": "Flood history not found!", "description": str(db_response)}, 200
         
-        return {"response": "success", "locations": [{"date": "2021/01/01", "rainfall": "30", "flood":"10"},{"date": "2021/01/01", "rainfall": "30", "flood":"10"}]}, 200
+
+    def post(self):
+        logger.debug("------------------------------------------------")
+        logger.info('/FloodHistory (Post) - '+str(request.remote_addr))
+        try:
+            request_body = request.json
+            date = request_body["date"]
+            rainfall = request_body["rainfall"]
+            flood = request_body["flood"]
+            geo = request_body["geo"]
+        except Exception as exception:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logger.error("\nException: "+str(exception)+"\nLine: "+str(exc_tb.tb_lineno))
+            return {"response": "error", "message": "Required fields are missing!"}, 403
+        
+        if(geo == "" or date == "" or rainfall == "" or flood == ""):
+            logger.error("Registraion failed - empty values\n")
+            return {"response": "error", "message": "One or more required fields are empty!"}, 403
+
+        insert_values = [(date, str(geo[0]), str(geo[1]), flood, rainfall)]
+        register_response = Insert("history", "date, geo_lat, geo_lng, flood, rainfall", "%s, %s, %s, %s, %s", insert_values)
+
+        if register_response == 1:
+            logger.info("Flood history recorded successfully")
+            return {"response": "success"}, 200
+        else:
+            logger.info("Flood history recording failed \n"+str(register_response))
+            return {"response": "failed", "message": "Flood history recording failed!", "description": str(register_response)}, 200
 
