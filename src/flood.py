1e8c1c04cfb7a471138806b32f04be00970baa07
Dinuka Navaratna <dinuka.navaratna@gmail.com>
2024-05-09 07:26:17 +0800

    severity

--- src/flood.py
+++ src/flood.py
@@ -72,7 +72,7 @@ class FloodHistory(Resource):
             locations = []
             for location in db_response:
                 locations.append({"id":str(location[0]), "date":str(location[1]), "geo":[float(location[2]), float(location[3])], "rainfall":str(location[4]), "flood":str(location[5])})
-            response["locations"] = locations
+            response["history"] = locations
             return response
         else:
             logger.info("Flood history failed \n"+str(db_response))
@@ -106,4 +106,54 @@ class FloodHistory(Resource):
         else:
             logger.info("Flood history recording failed \n"+str(register_response))
             return {"response": "failed", "message": "Flood history recording failed!", "description": str(register_response)}, 200
+
+
+class FloodSeverity(Resource):
+    def get(self):
+        logger.debug("------------------------------------------------")
+        logger.info('/FloodSeverity (Get) - '+str(request.remote_addr))
+
+        db_response = Select("id, address, geo_lat, geo_lng, severity", "severity", "", 0)
+
+        if(db_response == None):
+            return {"response": "failed", "message": "Flood severity not found!"}, 200
+        elif(type(db_response) is list):
+            response = {}
+            response["response"] = "success"
+            locations = []
+            for location in db_response:
+                locations.append({"id":str(location[0]), "address":str(location[1]), "geo":[float(location[2]), float(location[3])], "severity":str(location[4])})
+            response["severity"] = locations
+            return response
+        else:
+            logger.info("Flood severity failed \n"+str(db_response))
+            return {"response": "failed", "message": "Flood severity not found!", "description": str(db_response)}, 200
+        
+
+    def post(self):
+        logger.debug("------------------------------------------------")
+        logger.info('/FloodSeverity (Post) - '+str(request.remote_addr))
+        try:
+            request_body = request.json
+            address = request_body["address"]
+            severity = request_body["severity"]
+            geo = request_body["geo"]
+        except Exception as exception:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logger.error("\nException: "+str(exception)+"\nLine: "+str(exc_tb.tb_lineno))
+            return {"response": "error", "message": "Required fields are missing!"}, 403
+        
+        if(geo == "" or address == "" or severity == ""):
+            logger.error("Registraion failed - empty values\n")
+            return {"response": "error", "message": "One or more required fields are empty!"}, 403
+
+        insert_values = [(address, str(geo[0]), str(geo[1]), severity)]
+        register_response = Insert("severity", "address, geo_lat, geo_lng, severity", "%s, %s, %s, %s", insert_values)
+
+        if register_response == 1:
+            logger.info("Flood severity recorded successfully")
+            return {"response": "success"}, 200
+        else:
+            logger.info("Flood severity recording failed \n"+str(register_response))
+            return {"response": "failed", "message": "Flood severity recording failed!", "description": str(register_response)}, 200
 
--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,10 @@
 from .users import Register, Login
-from .flood import SafeLocations, FloodHistory
+from .flood import SafeLocations, FloodHistory, FloodSeverity
 
 def initialize_routes(api):
     api.add_resource(Register, "/register")
     api.add_resource(Login, "/login")
     api.add_resource(SafeLocations, "/safelocations")
     api.add_resource(FloodHistory, "/floodhistory")
+    api.add_resource(FloodSeverity, "/severity")
     
